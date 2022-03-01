76af33b5103ae98d5e733d3a7c4fdcd2193b3ba1
braum852 <julian.teo59@yahoo.com>
2022-03-01 13:33:38 +1000

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,24 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +32,14 @@ class Scraper
       end
     end
   end
-  
+
 end
 
+Scraper.new.print_courses
+##Then, we'll place a binding.pry on the next line. At the bottom of the file, outside of the class definition, we'll call Scraper.new.get_page. 
+#That way, we'll hit our binding and be able to play around with the HTML document in the terminal
+##Once your file looks like the code above, run the file with ruby lib/scraper.rb in your terminal. 
+##Once you hit your binding, type the doc variable into the terminal and you should see the HTML document, retrieved for us by Nokogiri and open-uri.
 
 
+#ruby lib/scraper.rb
